f210f19f3e694cd4b121955577b8a1239bb71754
Escafermo <31081737+Escafermo@users.noreply.github.com>
2018-01-12 09:17:46 -0200

    FirstTests
    Cameras, blocks, controls, cursors, raycasting, placeholders.

---
+++ Assets/Camera & UI/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject player;
+
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position;
+	}
+}
---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D enemyCursor = null;
+    [SerializeField] Texture2D questionCursor = null;
+    [SerializeField] Vector2 cursorHotSpot = new Vector2(96, 96); // Size of assets given by course
+
+    CameraRaycaster cameraRayCaster;
+
+    void Start ()
+    {
+        cameraRayCaster = GetComponent<CameraRaycaster>();
+    }
+
+    void Update()
+    {
+        switch (cameraRayCaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(enemyCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(questionCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            default:
+                Debug.LogError("Can't find cursor to show");
+                return;
+        }
+    }
+}
