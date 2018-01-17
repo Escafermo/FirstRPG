bd0b7fadfb1d628bac9320f8318669fc7de9ce82
Escafermo <31081737+Escafermo@users.noreply.github.com>
2018-01-17 15:27:32 -0200

    NewRaycaster&Assets

---
+++ Assets/_Main/Enemies/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float triggerRadius = 10f;
+
+    private float currentHealthPoints = 100;
+    AICharacterControl aiCharacter = null;
+    GameObject player = null;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacter = GetComponent<AICharacterControl>();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position,transform.position);
+
+        if (distanceToPlayer <= triggerRadius)
+        {
+            aiCharacter.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacter.SetTarget(null);
+        }
+    }
+
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / (float)maxHealthPoints;
+        }
+    }
+}
---
+++ Assets/_Main/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 100f;
+    
+    private float currentHealthPoints = 100;
+    
+    public float  healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / (float)maxHealthPoints;
+        }
+    }
+}
